5a3600b28904c90bfbd39083d9b35402009cd430
Edgar Machado <ed_2727@hotmail.com>
2019-03-06 13:36:32 -0800

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { SliderComponent } from './slider/slider.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
 	// tslint:disable-next-line:indent
-	declarations: [ AppComponent, SliderComponent ],
+	declarations: [ AppComponent ],
 	// tslint:disable-next-line:indent
 	imports: [
 		BrowserModule,
